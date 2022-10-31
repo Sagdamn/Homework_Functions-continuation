22baecb0cca6a82e67cb565e57927871bd1d12bd
Madina <sagdeeva.m.r@mail.ru>
2022-10-31 16:51:59 +0300

    Task 29 added

---
+++ Task_29/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит 
+их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+
+ввод элеменетов через консоль (через запятую. использовать метод string.Split()) */
+
+Console.WriteLine("Введите числа через запятую: ");
+string str = Console.ReadLine();
+string[] strArray = str.Split(',');
+
+int[] numArray = new int[strArray.Length];
+
+for (int i = 0; i < strArray.Length; i++)
+{
+    numArray[i] = int.Parse(strArray[i]);
+}
+
+Console.WriteLine("итоговый массив");
+
+for (int i = 0; i < numArray.Length; i++)
+{
+    if (i < numArray.Length - 1)
+        Console.Write($"{numArray[i]}, ");
+    else
+        Console.Write($"{numArray[i]}");
+}
